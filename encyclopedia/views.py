4b785bb185537cd885da3f1905aed6ff59887719
saad1551 <saad.ashraf.9094@gmail.com>
2023-08-18 07:31:20 +0500

    ADD url to the local urls and linekd with a template

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,7 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request):
+    return render(request, "encyclopedia/entry.html", {
+        "title": request.title
+    })
