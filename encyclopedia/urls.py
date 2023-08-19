74856a585b5fa343678e16f0b6e38a9b66aa7ae2
saad1551 <saad.ashraf.9094@gmail.com>
2023-08-19 15:26:32 +0500

    search functionality implemented

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("wikisearch", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from markdown import markdown
 from django.urls import reverse
+from .forms import MyForm
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
 
 from . import util
 
@@ -23,5 +26,37 @@ def index(request):
 def entry(request, title):
     return render(request, "encyclopedia/entry.html", {
         "title": title.capitalize(),
-        "content": markdown(util.get_entry(title))
+        "content": markdown(util.get_entry(title)) if util.get_entry(title) else "<h1>Sorry! The page you requested doesn't exist</h1>"
     })
+
+@require_POST
+def search(request):
+    form = MyForm(request.POST)
+    if form.is_valid():
+        search_string = form.cleaned_data['q']
+        titles = util.list_entries()
+        matchingTitles = []
+        for title in titles:
+            if search_string.lower() == title.lower():
+                return redirect(reverse("entry", kwargs={"title": title.lower()}))
+            elif title.lower().find(search_string.lower()) != -1:
+                    matchingTitles.append(title)
+        matchingEntries = []
+        matchingLinks = []
+        for matchingTitle in matchingTitles:
+            matchingLinks.append(reverse("entry", kwargs={"title": matchingTitle.lower()}))
+        for i in range(len(matchingTitles)):
+            matchingEntries.append({
+                "heading": matchingTitles[i],
+                "link": matchingLinks[i]
+            })
+        if len(matchingEntries) != 0:
+            return render(request, "encyclopedia/index.html", {
+                "entries": matchingEntries
+            })
+        else:
+            return render(request, "encyclopedia/error.html", {
+                "error": "<h3>No matching entries found</h3>"
+            })
+
+
